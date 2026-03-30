d3687538eedfe49c414b510c97137fc2fbfcc196
rafaelsampa <rafaelssampaiobr@gmail.com>
2026-03-30 13:08:55 -0300

    Imple. de interativo e Rotacao

--- project-2d/script.js
+++ project-2d/script.js
@@ -1,6 +1,8 @@
 
 
 
+
+
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
@@ -8,7 +10,7 @@ const W = canvas.width;
 const H = canvas.height;
 
 // =============================================================
-// VARIÁVEIS DE ESTADO (Agora fixas)
+// VARIÁVEIS DE ESTADO 
 // =============================================================
 let carroPosX = 100;
 let carroPosY = 150;
@@ -34,34 +36,66 @@ function desenhaMapa() {
 }
 
 function desenhaCarro() {
-  ctx.fillStyle = 'red';
-  ctx.fillRect(-20, -10, 40, 20); // Corpo do carro
-  ctx.fillStyle = 'lightblue';
-  ctx.fillRect(5, -8, 10, 16); //Para-brisa
+    ctx.fillStyle = 'red';
+    ctx.fillRect(-20, -10, 40, 20); // Corpo do carro
+    ctx.fillStyle = 'lightblue';
+    ctx.fillRect(5, -8, 10, 16); //Para-brisa
+
 }
 
 // =============================================================
-// RENDERIZAÇÃO DA CENA (sem loop?)
+// LOOP DE ANIMAÇÃO (Substitui o desenharCena)
 // =============================================================
-
-function desenharCena() {
-    // 1. Limpa a tela e reseta as tranformacoes 
+function animar() {
+    // 1. Limpa a tela e reseta as tranformacoes a cada frame
     ctx.setTransform(1, 0, 0, 1, 0, 0);
     ctx.clearRect(0, 0, W, H);
 
-    // 2. Desenha o fundo
+    // 2. Desenha o fundo estático
     desenhaMapa();
-    desenhaCarro(); // Antes
 
-    // 3. Desenha o carro 
+    // 3. Aplica as transformações dinâmicas e desenha o carro 
     ctx.save();
     ctx.translate(carroPosX, carroPosY);
     ctx.rotate(carroAngulo);
+
     desenhaCarro();
+
     ctx.restore();
+
+    // 4. Pede ao navegador para chamar essa função no próximo frame
+    requestAnimationFrame(animar);
 }
 
-desenharCena();
+// =============================================================
+// INTERATIVIDADE
+// =============================================================
+document.addEventListener('keydown', function(e) {
+  const velocidade = 5; // pixels que o carro anda por clique
+  switch (e.key) {
+    case 'ArrowLeft':  
+        carroPosX -= velocidade; 
+        carroAngulo = Math.PI; 
+        break;
+    case 'ArrowRight': 
+        carroPosX += velocidade; 
+        carroAngulo = 0; 
+        break;
+    case 'ArrowUp':    
+        carroPosY -= velocidade; 
+        carroAngulo = -Math.PI/2; 
+        break;
+    case 'ArrowDown':  
+        carroPosY += velocidade; 
+        carroAngulo = Math.PI/2; 
+        break;
+  }
+});
+
+animar();
+
+
+
 
 
 
